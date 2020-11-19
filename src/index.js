2c410c60f136ddc876c453111b8b7717dfef519a
MH Hamza <mhamza2551@gmail.com>
2020-11-19 11:32:52 +0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import Parent from './parent';
+
+ReactDOM.render(<Parent />, document.getElementById('root'))
